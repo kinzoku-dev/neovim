45055e95578d6232510e564963d06794828db72f
kinzoku-dev <kinzokudev4869@gmail.com>
2023-11-22 12:52:37 -0600

    add markdown-preview.nvim

--- lua/plugins/markdownpreview.lua
+++ lua/plugins/markdownpreview.lua
@@ -1,5 +1,8 @@
 return {
-  'ellisonleao/glow.nvim',
-  config = true,
-  cmd = 'Glow',
+  'iamcco/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  ft = { 'markdown' },
+  build = function()
+    vim.fn['mkdp#util#install']()
+  end,
 }
