19387bb8ad82135112ca86552e0c6467001eb3e9
kinzoku-dev <kinzokudev4869@gmail.com>
2023-12-10 11:04:16 -0600

    make catppuccin transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,6 +6,7 @@ return {
     priority = 1000,
     config = function()
       require('catppuccin').setup {
+        transparent_background = true,
         color_overrides = {
           all = {
             base = '#11111b',
